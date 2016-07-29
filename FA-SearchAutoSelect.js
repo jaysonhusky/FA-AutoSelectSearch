bb06c3be1cc38c4aa5f0846c8c4dcf9d3e1492d6
jaysonhusky <jaysonhusky@live.com>
2016-07-29 10:30:42 +0100

    Update FA-SearchAutoSelect.js

--- FA-SearchAutoSelect.js
+++ FA-SearchAutoSelect.js
@@ -18,10 +18,10 @@
 	// Find ALL the checkboxes and select them (It can be narrowed down to just the ratings if required)
 	MarkAllBoxes([].slice.call(document.querySelectorAll('input[type="checkbox"][name*="rating"]')));
 
-	// Set Mutuation Handler for them
+	// Set Mutation Handler for them
 	setMutationHandler(document, 'input[type="checkbox"]', MarkAllBoxes);
 
-	// Function for Mutuation
+	// Function for Mutation
 	function MarkAllBoxes(nodes) {
 		nodes.forEach(function(n){n.checked = true;});
 	}
